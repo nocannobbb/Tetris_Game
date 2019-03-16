0956a261151e42917e8abd0d772a87d0e6135644
nocannobbb <43512114+nocannobbb@users.noreply.github.com>
2019-03-16 16:46:24 +0800

    My original Teris Game
    learn from "How to make a game like aTetris in unity5" by The Weekly Coder
    
    Link：https://www.youtube.com/watch?v=dckLkKfTYMk&list=PLiRrp7UEG13axMHD7Kqdiy30c7ZBu_Zn7&index=8

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,305 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    //grid 
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    //Score
+    public int oneLineScore = 40;
+    public int twoLineScore = 100;
+    public int threeLineScore = 400;
+    public int fourLineScore = 1000;
+
+    // number of full line
+    private int numberOfLineThisTurn = 0;
+
+    //the text of score, correspond to the text in the canvas
+    public Text hud_score;
+
+    //the score variable that users now get
+    public static int currentScore = 0;
+
+    // the entity of grid
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+
+    //sound player
+    public AudioSource audiosource;
+    //when lines are cleared, the sound will be played
+    public AudioClip clearLineSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawn the first tetromino
+        SpawnNextTetromino();
+        //init the sound player, GetComponent means get the component in the inspector
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        //to update users' ectra source
+        UpdateScore();  
+
+        //update the score shown in the scene
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        // match the text component and the score variable
+        hud_score.text = currentScore.ToString();
+        
+    }
+
+    public void UpdateScore()
+    {
+        if(numberOfLineThisTurn > 0)
+        {
+            //call method according to the number of full lines
+            if(numberOfLineThisTurn == 1)
+            {
+
+                ClearOneLine();
+
+            }else if(numberOfLineThisTurn == 2)
+            {
+
+                ClearTwoLine();
+
+            }else if(numberOfLineThisTurn == 3)
+            {
+
+                ClearThreeLine();
+
+            }else if(numberOfLineThisTurn == 4)
+            {
+
+                ClearFourLine();    
+                
+            }
+            numberOfLineThisTurn = 0;
+            PlayLineCleardSound();
+        }
+    }
+
+    //add the corresponding score in each function
+    public void ClearOneLine()
+    {
+        currentScore += oneLineScore;
+    }
+
+    public void ClearTwoLine()
+    {
+        currentScore += twoLineScore;
+    }
+
+    public void ClearThreeLine()
+    {
+        currentScore += threeLineScore;
+    }
+
+    public void ClearFourLine()
+    {
+        currentScore += fourLineScore;
+    }
+
+    public void PlayLineCleardSound()
+    {
+        audiosource.PlayOneShot(clearLineSound);
+    }
+
+    //border check
+    public bool CheckIfAboveGrid(Tetromino tetromino)
+    {
+        for(int x = 0; x < gridWidth; ++x)
+        {
+            foreach(Transform mino in tetromino.transform)
+            {
+                Vector2 pos = mino.position;
+                if(pos.y > gridHeight - 1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // check if line is filled of position y
+    public bool IsFullAtRoW(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            // not full in this line
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+        numberOfLineThisTurn++;
+        return true;
+
+    }
+
+    // destroy all the minos in the line of y
+    public void DestroyMinoAtRow(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    //move all the object down above y
+    public void MoveRowDown(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            if(grid[x,y] != null)
+            {
+                // get object down, but position is not changed
+                grid[x, y - 1] = grid[x, y];
+                //set original pos to null 
+                grid[x, y] = null;
+                //change the position
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+    public void MoveAllRowsDown(int y)
+    {
+        //for all objects above y, move them down
+        for (int i = y; i < gridHeight; ++i)
+        {
+            MoveRowDown(i);
+        }
+    }
+
+    public void DeleteRow()
+    {
+        // for all vertical position,check if there are full rows, if there are, destroy them and move them all down
+        for(int y = 0; y < gridHeight; ++y)
+        {
+            if(IsFullAtRoW(y))
+            {
+                DestroyMinoAtRow(y);
+                MoveAllRowsDown(y + 1);
+                // y move been moved down,so y = y - 1
+                y--;    
+            }
+        }
+
+    }
+
+    public void UpdateGrid(Tetromino tetromino)
+    {
+        //not clear now
+        for (int y = 0; y < gridHeight; ++y )
+        {
+            for(int x = 0; x < gridWidth; ++x)
+            {
+                if(grid[x,y] != null)
+                {
+                    if(grid[x,y].parent == tetromino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+        }
+
+        // set the grid with the minos in transform
+        foreach(Transform mino in tetromino.transform)
+        {
+            Vector2 pos = GetInteger(mino.position);
+            if(pos.y < gridHeight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+
+    //return back the object of the position in the grid
+    public Transform GetTransformAtGridPosition(Vector2 pos)
+    {
+        //illegal
+        if(pos.y > gridHeight - 1)
+        {
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+        }
+    }
+
+    public void SpawnNextTetromino()
+    {
+        //instantiate prefabs from the prefabs folder
+            GameObject nextTetromino = Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject))
+                , new Vector3(5.0f, 20.0f,0.0f), Quaternion.identity) as GameObject;
+  
+    }
+
+    //check the boundary
+    public bool CheckEdgePosition(Vector3 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+
+    //return the integer of the pos
+    public Vector2 GetInteger(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    //randomly produce the name of the prefabs, then return to instantiate the object
+     string GetRandomTetromino()
+    {
+        int randomTetromino = Random.Range(1, 8);
+        string tetrominoName = "Prefabs/J";
+
+        switch(randomTetromino)
+        {
+            case 1:
+                tetrominoName = "Prefabs/J";
+                break;
+            case 2:
+                tetrominoName = "Prefabs/L";
+                break;
+            case 3:
+                tetrominoName = "Prefabs/Long";
+                break;
+            case 4:
+                tetrominoName = "Prefabs/S";
+                break;
+            case 5:
+                tetrominoName = "Prefabs/Square";
+                break;
+            case 6:
+                tetrominoName = "Prefabs/T";
+                break;
+            case 7:
+                tetrominoName = "Prefabs/Z";
+                break;
+            
+        }
+        //Debug.LogError(tetrominoName);
+        return tetrominoName;
+    }
+
+    //call the GameOver Scene
+    public void Gameover()
+    {
+        Application.LoadLevel("GameOver");
+    }
+
+
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+
+    
+    float fall = 0; // time record of last movement
+    public float fallspeed = 1; // the fall speed
+
+    //the limitation property of tetromino to rotate
+    public bool allowrotation = true; 
+    public bool limitrotation = false;
+
+    // the higher speed users use to fall, the high score they will get
+    public int individualScore = 100;   //the full extra score  
+    private float individualScoreTime;  //the timer, like the contain, once overflow, operate something
+
+    //sound effects for tetromino
+    public AudioClip moveSound;     //moveSound effect, correspond to the component in the inspector
+    public AudioClip rotateSound;   //rotateSound effect, correspond to the component in the inspector
+    public AudioClip landSound;     //landSound effect, correspond to the component in the inspector
+
+    private AudioSource audioSource;    //Sound Player, to play sound above
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();  // get entity of component in the inspector in the initiation
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+        UpdateIndividualScore();
+    }
+
+    public void UpdateIndividualScore()
+    {
+        //count the extra score that the users can get
+        if(individualScoreTime < 1) //cut the period into every unit
+        {
+            individualScoreTime += Time.deltaTime;  //plus deltaTime every update
+
+        }
+        else
+        {
+            individualScoreTime = 0;    //if individualScoreTime >= 1,means user use more than unit time
+            individualScore = Mathf.Max(individualScore - 10, 0); //then minus 10 score, and keep updating
+        }
+    }
+
+    //check user's input function
+    void CheckUserInput()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            transform.position += new Vector3(1, 0, 0);
+
+            if (CheckIfValid())     //Check if after moving. the object is still within border
+            {
+                //if it is, update the grid in Game
+                //FindObjectOfType means find object of the whole scene, differ from GetComponent<>
+                FindObjectOfType<Game>().UpdateGrid(this);
+                //once update,means valid movement,then play the sound
+                playMoveSound();                            
+            }
+            else
+            {
+                //if not, get back to the original position(eyes cannot discover)
+                transform.position -= new Vector3(1, 0, 0); 
+            }
+
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            transform.position += new Vector3(-1, 0, 0);
+            if (CheckIfValid())
+            {
+                FindObjectOfType<Game>().UpdateGrid(this);
+                playMoveSound();
+            }
+            else
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //here to limit the rotation of some objects that can just rotate in 180 degree, and be back
+            if(allowrotation)
+            {
+                if(limitrotation)
+                {
+                    if(transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+
+                if (CheckIfValid())
+                {
+                    FindObjectOfType<Game>().UpdateGrid(this);
+                    playRotateSound();
+                }
+                else
+                {
+                    if (limitrotation)
+                    {
+                        if (transform.eulerAngles.z >= 90)
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, 90);
+                        }
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                }
+
+            }
+            
+
+        }
+        //keep object keeping being down with and without pressing button
+        //use fall to record the last movement,and when Time.time grows,Time.time grows
+        //finally when their gap is lager than fallspeed, the falling sentence is been executed
+        else if(Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallspeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            // to record the last moving time
+            fall = Time.time;   
+
+            if (CheckIfValid())
+            {
+                FindObjectOfType<Game>().UpdateGrid(this);  
+                if(Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    playMoveSound();
+                }
+            }
+            else
+            {
+                transform.position -= new Vector3(0, -1, 0);
+
+                //check if there is some rows filled, if they are, delete them
+                FindObjectOfType<Game>().DeleteRow();   
+
+                //check if there is some objects beyond Height - 1
+                if(FindObjectOfType<Game>().CheckIfAboveGrid(this))
+                {
+                    FindObjectOfType<Game>().Gameover();
+                }
+
+                //freeze the objects not to move
+                enabled = false;
+
+                playLandSound();
+
+                //spawn the next tetromino
+                FindObjectOfType<Game>().SpawnNextTetromino();
+
+                // get the extra score added(currentScore is a static variable)
+                Game.currentScore += individualScore;
+
+                
+            }
+
+        }
+    }
+
+    //audio played function
+    void playMoveSound()
+    {
+        audioSource.PlayOneShot(moveSound);
+    }
+
+    void playLandSound()
+    {
+        audioSource.PlayOneShot(landSound);
+    }
+
+    void playRotateSound()
+    {
+        audioSource.PlayOneShot(rotateSound);
+    }
+
+    //check if the objects are beyond edge
+    bool CheckIfValid()
+    {
+        foreach (Transform mino in transform)
+        {
+            //get the position (x,y) from float to integer
+            Vector2 pos = (FindObjectOfType<Game>().GetInteger(mino.position));
+
+            //check the edge situation
+            if (FindObjectOfType<Game>().CheckEdgePosition(pos) == false)
+            {
+                return false;
+            }
+
+            //if there is transform in grid[x,y], and it does not match with the parent of the grid[x,y] 
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null &&
+                FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
+            {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+}
